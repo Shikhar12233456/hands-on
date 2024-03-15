0125a6dc49b5cfff3c37c671b0f1c6da76937a67
ar-shikhar.awasthi <ar-shikhar.awasthi@rakuten.com>
2024-03-15 14:22:20 +0530

    new project

--- src/main/java/com/training/pms/handsonjenkins/HandsOnJenkinsApplication.java
+++ src/main/java/com/training/pms/handsonjenkins/HandsOnJenkinsApplication.java
@@ -2,12 +2,19 @@ package com.training.pms.handsonjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HandsOnJenkinsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HandsOnJenkinsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HandsOnJenkinsApplication.class, args);
+    }
+    @GetMapping
+    public String getMessage()
+    {
+        return "Hello From Shikhar";
+    }
 }
--- src/test/java/com/training/pms/handsonjenkins/HandsOnJenkinsApplicationTests.java
+++ src/test/java/com/training/pms/handsonjenkins/HandsOnJenkinsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HandsOnJenkinsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
