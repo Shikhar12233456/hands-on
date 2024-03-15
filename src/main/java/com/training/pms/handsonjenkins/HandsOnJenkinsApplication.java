c2809f0472001c4eafc53fd070559a4f62af4be6
ar-shikhar.awasthi <ar-shikhar.awasthi@rakuten.com>
2024-03-15 14:21:32 +0530

    first commit

---
+++ src/main/java/com/training/pms/handsonjenkins/HandsOnJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.training.pms.handsonjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/pms/handsonjenkins/HandsOnJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.pms.handsonjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
